662aa544468d80a35c45ab4f309f67b485a0953c
AndreaNaspi <andreanaspi@gmail.com>
2017-11-20 15:01:01 +0100

    versione 2.0 initStorge
    Versione 2.0 di initstorage (vedere i commenti per la nuova versione)

--- Progetto/js/initStorage.js
+++ Progetto/js/initStorage.js
@@ -3,8 +3,14 @@ Questa funzione deve essere eseguita onload della pagina index.html (pagina home
 
 STRUTTURA ATTUALE DEL LOCAL STORAGE: (sarà aggiornata con il tempo)
 	-localStorage.pages:array di Object, tali Object sono formati da un id (stringa rappresentante il titolo dell'articolo), un campo text (stringa rappresentante il testo dell'articolo) e un campo likedBy
-	 contenente un array con gli id univoci degli utenti che hanno messo like a tale articolo
-
+    contenente un array con gli id univoci degli utenti che hanno messo like a tale articolo
+    
+VERSIONE 2.0: (ricordarsi sempre di eseguire tale js on load della prima pagina)
+    è stato aggiunto un nuovo indirizzo a localStorage chiamato localStorage.users contenente un array di Object.
+    Tali object sono formati da username, email e password (hashata univocamente in initUser.js). 
+    Questi dati devono essere richiesti al momento della registrazione. 
+    Il login verrà eseguito tramite du campi: primo campo username/email e secondo campo password    
+    
 **/
 function init()
 {
@@ -12,12 +18,17 @@ function init()
     {
         localStorage.pages=JSON.stringify([]);
     }
+    if (typeof(localStorage.users) == "undefined") 
+    {
+        localStorage.users=JSON.stringify([{username:"admin",email:"andreanaspi@gmail.com",password:"1303378837"}]);
+    }
+
     var newPages = [{id:"HTML", likedBy:['u4'], text:"This is the html page, in the body tag you can use javascript or jquery to do some scripts. Javascript is beatiful."},
                     {id:"JavaScript", likedBy:['u1','u2'], text:"This is the javascript page, this code is write inside the html tag style. Jquery is an extension of javascript"},
                     {id:"JQuery", likedBy:['u3'], text:"This is the JQuery page."}];
     var pages = JSON.parse(localStorage.pages);
                 
-     if(newPages.length != pages.length)
+    if(newPages.length != pages.length)
     {
         localStorage.pages = JSON.stringify(newPages);
     }
